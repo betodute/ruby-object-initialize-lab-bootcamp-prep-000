4d3ff8f3afed68f3c35c3c6222862dbb5c68d953
Beto Dute <betodute@gmail.com>
2019-05-12 23:02:20 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -11,18 +11,6 @@ class Person
   def name
     @name
   end
-  
-  def initialize (breed)
-    @breed = breed
-  end
-  
-  def breed= (breed = "Mutt")
-    @breed = breed
-  end
-  
-  def breed
-    @breed
-  end
 
 end
 
